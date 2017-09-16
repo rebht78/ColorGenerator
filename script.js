4fc97534ae8af1c77b1b7074339f386dbced2c92
Sohel Shaikh <rebht78@gmail.com>
2017-09-16 16:47:25 +0530

    Updated script.js
    Added Validation feature to it

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
-window.onkeydown = function(e)
-{
-	//getting the div with id keycode
-	var divKeyCode = document.querySelector('#keycode');
-	//getting the div with id actualkey
-	var divActual = document.querySelector('#actualkey');
+/*
+declaration of variables and assigning value from TextBoxes
+*/
+var divBox = document.querySelector('.box');
+var redColor = document.getElementById('txtRedColor').value;
+var greenColor = document.getElementById('txtGreenColor').value;
+var blueColor = document.getElementById('txtBlueColor').value;
 
-	divKeyCode.innerHTML = e.keyCode;
-	divActual.innerHTML = e.key;
-};
+/*
+validate function to check whether input has been given by the user or not.
+*/
+function validate()
+{
+	if (!redColor || !greenColor || !blueColor)
+	{
+		return false;
+	}
+	return true;
+}
+// click listener to handle click on button - Show Color
+function btnSubmitClickListener()
+{
+	//call to validate, if it returns true then call generateColor
+	if (validate())
+	{
+		generateColor();
+		return true;
+	}
+	else
+	{
+		alert("Please enter the value of Code");
+		return false;
+	}
+}
+// generateColor will assign to div - box style of background color and innerHTML
+function generateColor()
+{
+
+	divBox.setAttribute("style", "background-color:#"+redColor+greenColor+blueColor);
+	divBox.innerHTML = "#".concat(redColor).concat(greenColor).concat(blueColor);
+
+}
+// click listener to handle click on button - Reset Code
+function btnResetClickListener()
+{
+	document.getElementById('txtRedColor').value = "";
+	document.getElementById('txtGreenColor').value = "";
+	document.getElementById('txtBlueColor').value = "";
+	document.getElementById('txtRedColor')
+	.setAttribute("placeholder", "Enter Red Color Code");
+	document.getElementById('txtGreenColor')
+	.setAttribute("placeholder", "Enter Green Color Code");
+	document.getElementById('txtBlueColor')
+	.setAttribute("placeholder", "Enter Blue Color Code");
+	divBox.style.visibility = "hidden";
+}
